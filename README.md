80b7aaaa28e79bf0469630af92feab7c7ee376f2
Chaitanya Ravuri <cravuri@mit.edu>
2023-12-08 14:16:04 -0500

    Updated terminal UI

--- README.md
+++ README.md
@@ -4,9 +4,33 @@ Allows an LLM to explore your filesystem and read code files. The memory system 
 
 ## Setup
 
-Install the requirements, and add your OpenAI API key.
+Clone the directory:
 
 ```
+git clone https://github.com/20ChaituR/hierarchical-llm-memory.git
+cd hierarchical-llm-memory
+```
+
+Setup the environment, installing all the necessary libraries:
+
+```
+python -m venv venv
+source venv/bin/activate
 pip install -r requirements.txt
+```
+
+Follow the instructions [here](https://help.openai.com/en/articles/4936850-where-do-i-find-my-api-key) to get an OpenAI API key. Add the key to your environment:
+
+```
 export OPENAI_API_KEY='your-api-key-here'
 ```
+
+## Running the Code
+
+To run the code, run the following command:
+
+```
+python main.py your_directory "your query"
+```
+
+Where `your_directory` should be the directory which you want ChatGPT to have access to, and `your query` is the question you want to ask about that directory.
--- main.py
+++ main.py
@@ -1,20 +1,22 @@
+import argparse
 import json
-import pyperclip
+import os
+import sys
+import time
 
-from memory import *
+from memory import Directory
 from openai import OpenAI
-from typing import List
-
 
 # TODO:
+# - Use urwid for terminal output handling
 # - Right now it only supports query -> response, no chat functionality
 #     - Can possibly implement chat by replacing {query} with chat history
 #       (so instead of just a user query, it is the full chat history of user
 #       messages and AI messages)
 
 MODEL = "gpt-4-1106-preview"
+# MODEL = "gpt-3.5-turbo-1106"
 FILE_TOKEN_LIMIT = 1000
-THOUGHT_TOKEN_LIMIT = 1000
 
 MESSAGE = """A user has asked the following query: "%s"
 
@@ -47,50 +49,113 @@ You have already been exploring this code for some time, and the following is a 
 
 Please give a single JSON response, in either the first format or the second format, based on the file structure and your previous thoughts."""
 
+import curses
 
-def num_tokens(messages: List) -> int:
-    encoding = tiktoken.encoding_for_model(TOKENIZER_MODEL)
-    count = 0
-    for message in messages:
-        tokens = encoding.encode(message)
-        count += len(tokens)
-    return count
+_num_lines = 0
 
 
-root_dir = "/Users/cravuri/Projects/MIT/21M.385/banger"
-query = """Right now, the IntroScreen button is slightly off-center. Can you tell me exactly what to do to make it exactly centered on screen?"""
+def clear_and_print(stdscr, s: str) -> None:
+    stdscr.clear()
+    stdscr.scrollok(True)  # Enable scrolling in the window
+    stdscr.idlok(True)  # Enable intelligent scrolling
+    height, width = stdscr.getmaxyx()
+    lines = s.split("\n")
 
-root = Directory(root_dir)
-client = OpenAI()
-summary = "To solve the user's query, I need to explore the file structure to find the relevant files. So far, I have opened the root directory and observed the top level structure. Nothing unexpected has happened, and I should continue exploring the user's file structure."
-root.open(root_dir)
-while True:
-    message = MESSAGE % (query, root.to_str(), SUMMARY % summary)
-    response = client.chat.completions.create(
-        model=MODEL,
-        response_format={"type": "json_object"},
-        messages=[{"role": "user", "content": message}],
+    i = 0
+    for line in lines:
+        wrapped_lines = [
+            line[i : i + width] for i in range(0, len(line), width)
+        ]  # Wrap long lines
+        for wrapped_line in wrapped_lines:
+            if (
+                stdscr.getyx()[0] >= height - 1
+            ):  # Check if we're at the bottom of the window
+                stdscr.scroll()  # Scroll up if at the bottom
+                i -= 1
+            stdscr.addstr(i, 0, wrapped_line)
+            i += 1
+    stdscr.refresh()
+
+
+# def clear_and_print(stdscr, s: str) -> None:
+#     stdscr.clear()
+#     lines = s.split("\n")
+#     for i, line in enumerate(lines):
+#         stdscr.addstr(i, 0, line)
+#     stdscr.refresh()
+
+# global _num_lines
+# for _ in range(_num_lines):
+#     sys.stdout.write("\x1b[1A")
+#     sys.stdout.write("\x1b[2K")
+
+# count = len(s.split("\n"))
+# _num_lines = count
+# print(s)
+
+
+def main(stdscr, root_dir, query):
+    stdscr.clear()
+    stdscr.refresh()
+
+    root = Directory(root_dir)
+    client = OpenAI()
+    summary = f"To solve the user's query, I need to explore the file structure to find the relevant files. I currently only see the root directory, {os.path.basename(root_dir)}/, so I should expand that directory to explore it further."
+    while True:
+        message = MESSAGE % (query, root.to_str(), SUMMARY % summary)
+        response = client.chat.completions.create(
+            model=MODEL,
+            response_format={"type": "json_object"},
+            messages=[{"role": "user", "content": message}],
+        )
+        assistant_message = response.choices[0].message.content
+        # assistant_message = '{"command": 1}'
+        content = json.loads(assistant_message)
+
+        if "message" in content:
+            curses.nocbreak()
+            stdscr.keypad(False)
+            curses.echo()
+            curses.endwin()
+            print(content["message"])
+            # clear_and_print(stdscr, content["message"])
+            break
+        elif "command" in content:
+            command = content["command"]
+            root.open_by_index(command)
+            while root.size() > FILE_TOKEN_LIMIT:
+                root.close_oldest()
+
+        user_response = RESPONSE % (root.to_str(), query)
+        response = client.chat.completions.create(
+            model=MODEL,
+            response_format={"type": "text"},
+            messages=[
+                {"role": "user", "content": message},
+                {"role": "assistant", "content": assistant_message},
+                {"role": "user", "content": user_response},
+            ],
+        )
+        summary = response.choices[0].message.content
+        # summary = query
+        time.sleep(5)
+        clear_and_print(stdscr, f"{root.to_str()}\nSUMMARY: {summary}")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Answer any query about a codebase.")
+    parser.add_argument(
+        "root_dir", type=str, help="The root directory of your codebase."
     )
-    content = json.loads(response.choices[0].message.content)
-
-    print(message)
-    print(content)
-
-    if "message" in content:
-        break
-    elif "command" in content:
-        command = content["command"]
-        root.open_by_index(command)
-        while root.size() > FILE_TOKEN_LIMIT:
-            root.close_oldest()
-
-    response = client.chat.completions.create(
-        model=MODEL,
-        response_format={"type": "text"},
-        messages=[
-            {"role": "user", "content": message},
-            {"role": "assistant", "content": response.choices[0].message.content},
-            {"role": "user", "content": RESPONSE % (root.to_str(), query)},
-        ],
+    parser.add_argument(
+        "query", type=str, help="The question you have about your codebase."
     )
-    summary = response.choices[0].message.content
+
+    args = parser.parse_args()
+
+    stdscr = curses.initscr()
+    curses.noecho()
+    curses.cbreak()
+    stdscr.keypad(True)
+    main(stdscr, args.root_dir, args.query)
+    # main(args.root_dir, args.query)
